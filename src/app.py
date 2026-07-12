1ea63ff78daf6ea08c623ba86f17a7605f5983f0
Sangam Basnet <basnetsangam86@gmail.com>
2026-07-12 12:33:41 +0545

    making app.py extremely efficient ith controller

--- src/app.py
+++ src/app.py
@@ -1,23 +1,4 @@
-import streamlit as st
+from ui import render_page
 
-st.set_page_config(
-    page_title="ResearchViz",
-    page_icon=":bar_chart:",
-    layout="centered"
-)
-
-st.title("ResearchViz")
-
-st.subheader("Automatic CSV cleaning & visualization Tool for Researchers")
-
-st.write(
-    """
-    Upload your csv dataset and let ResearchViz automatically:
-    - Clean your dataset
-    - Analyze your data
-    - Generate visualizations
-    - Create a professional PDF report"""
-)
-
-uploaded_file = st.file_uploader("Choose a csv file", type="csv")
-
+if __name__ == "__main__":
+    render_page()
